9abac4d2e651797dbc6b649b593725d17a08f781
ameer-ahmad <ameer5ahmad@gmail.com>
2022-04-17 19:31:22 -0400

    Updates

---
+++ precache-manifest.d12c28a7e8e98a6352c8c8a232463672.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ae8665fc10d7bc67f8457aa9247881",
+    "url": "/hang-man/index.html/index.html"
+  },
+  {
+    "revision": "1490ab54d723172e7890",
+    "url": "/hang-man/index.html/static/css/main.fe55eaba.chunk.css"
+  },
+  {
+    "revision": "d0632374f1df33b2a1bd",
+    "url": "/hang-man/index.html/static/js/2.e42cc546.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/hang-man/index.html/static/js/2.e42cc546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1490ab54d723172e7890",
+    "url": "/hang-man/index.html/static/js/main.04a004de.chunk.js"
+  },
+  {
+    "revision": "7772edc2f2cbe545be5d",
+    "url": "/hang-man/index.html/static/js/runtime-main.73e70897.js"
+  }
+]);
